344ca7bd376de6c890470e51595d29cf590c5a68
jwh0124 <jwh0124@gmail.com>
2024-01-23 19:59:11 +0900

    redis prototype project initialized

---
+++ redis/src/test/java/edu/circle/prototype/redis/RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.circle.prototype.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
