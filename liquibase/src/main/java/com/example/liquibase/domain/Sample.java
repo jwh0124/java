b3b0600161da2992a03b0a33c3c98fe97e52db8e
Circle Jung <jwh0124@gmail.com>
2025-11-20 15:55:04 +0900

    liquibase prototype

---
+++ liquibase/src/main/java/com/example/liquibase/domain/Sample.java
@@ -0,0 +1,21 @@
+package com.example.liquibase.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Sample {
+
+    @Id
+    @Column(name = "id")
+    private long id;
+}
